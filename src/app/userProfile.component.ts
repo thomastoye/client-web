cad3048ba38c38c18a94808897a1e6bf52652a7d
PERRINN <perrinnlimited@gmail.com>
2018-03-16 15:13:10 +0000

    UI

--- src/app/userProfile.component.ts
+++ src/app/userProfile.component.ts
@@ -35,7 +35,7 @@ import { databaseService } from './database.service';
       </div>
       <div>
       <div style="float:left;margin-top:10px;color:#222">{{DB.getTeamName(team.$key)}}{{(DB.getTeamLeader(team.$key,UI.focusUser)?" *":"")}}</div>
-      <img [hidden]="!(DB.getTeamBalance(team.$key)>0)" src="./../assets/App icons/PERRINN-icon-180x180.png" style="float:left;height:12px;margin:5px;margin-top:14px">
+      <img [hidden]="!(DB.getTeamBalance(team.$key)>0)" src="./../assets/App icons/icon_share_03.svg" style="float:left;height:17px;margin:5px;margin-top:12px">
       <div *ngIf="DB.getUserPersonalTeam(UI.focusUser)==team.$key" style="float:left;margin-top:10px;color:green;font-size:11px;background-color:#eee;width:90px;text-align:center">Personal team</div>
       <div style="float:left;margin:5px;margin-top:14px;background-color:red;width:12px;height:12px;border-radius:6px" *ngIf="DB.getTeamLastMessageTimestamp(team.$key)>team.lastChatVisitTimestamp"></div>
       <div style="float:right;margin-top:10px;color:#999;margin-right:10px">{{team.lastChatVisitTimestamp|date:'d MMM'}}</div>
