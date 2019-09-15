85ffc146dfa4ecb76acf29e5614c9a8e50bea607
PERRINN <perrinnlimited@gmail.com>
2019-09-15 22:08:42 +0100

    Update userProfile.component.ts

--- src/app/userProfile.component.ts
+++ src/app/userProfile.component.ts
@@ -11,8 +11,8 @@ import { userInterfaceService } from './userInterface.service';
   template: `
   <div id='main_container'>
   <div style="max-width:800px;margin:0 auto">
-    <img class='editButton' style="float:left;width:20px" [hidden]='!(UI.currentUser==UI.focusUser)' (click)="this.router.navigate(['userSettings',UI.focusUser])" src="./../assets/App icons/settings.png">
-    <div style="float:right;width:22px;height:22px;text-align:center;line-height:18px;font-size:18px;margin:5px;color:blue;border-color:blue;border-width:1px;border-radius:11px;border-style:solid;cursor:pointer">+</div>
+    <img class='editButton' style="float:left;width:25px" [hidden]='!(UI.currentUser==UI.focusUser)' (click)="this.router.navigate(['userSettings',UI.focusUser])" src="./../assets/App icons/settings.png">
+    <div style="float:right;width:26px;height:26px;text-align:center;line-height:22px;font-size:22px;margin:5px;color:blue;border-color:blue;border-width:1px;border-radius:13px;border-style:solid;cursor:pointer">+</div>
   </div>
   <div class='sheet'>
   <div class="spinner" *ngIf="UI.loading">
